260deace39a063f449552f7192b1903d6d102e99
saiminh <saiminh@gmail.com>
2024-06-26 19:46:12 +0200

    initial commit

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5a3d4b621cf69549da55');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1de46a8861015490b83a');
