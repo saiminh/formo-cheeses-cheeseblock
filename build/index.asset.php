c20470e42551635a23bbd6c464a3497597a33bea
saiminh <saiminh@gmail.com>
2024-06-26 12:52:44 +0200

    updated content width

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e3e54142a4020ef0bfc9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5a3d4b621cf69549da55');
